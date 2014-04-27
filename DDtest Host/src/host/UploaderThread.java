bed15f8a2bfee2bb335d8a47fe62daefb498ba88
Hammer Editor <support@hammereditor.net>
2014-04-27 13:24:35 -0400

    Init

--- DDtest Host/src/host/UploaderThread.java
+++ DDtest Host/src/host/UploaderThread.java
@@ -1,10 +1,14 @@
 package host;
 
+import java.io.*;
+import java.net.*;
+
 public class UploaderThread implements Runnable 
 {
 	//class handles a data channel for uploading a file
-	public UploaderThread(int port, File dataF, int throughputSim) //throughputSim represents Mbps. Value of 4 = 4 Mbps simulation.
+	public UploaderThread(int port, String clientIPaddr, File dataF, int throughputSim) //throughputSim represents Mbps. Value of 4 = 4 Mbps simulation.
 	{
+		//clientIPaddr is used to verify that the correct client has connected.
 		
 	}
 }
