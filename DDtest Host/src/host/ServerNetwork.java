84b724b8815cd378533fe0462ac6f120464e52c9
Hammer Editor <support@hammereditor.net>
2014-04-28 18:30:06 -0400

    S

--- DDtest Host/src/host/ServerNetwork.java
+++ DDtest Host/src/host/ServerNetwork.java
@@ -14,7 +14,7 @@ public class ServerNetwork
 		//each host can hold 400 MB of data. total capacity: 9600 MB, capacity when mirroring is factored in: 1600 MB.
 		//goal: mirror data in this fashion: http://www.hammereditor.net/images/globaldisk_24host_scheme.png
 		
-		testA();
+		testB_100k();
 
 	}
 	
@@ -51,18 +51,72 @@ public class ServerNetwork
 		
 		System.out.println();
 		System.out.println("Stripe availability: ------------");
+		
+		
 		for (int i = 0; i < downHosts.length; i++)
 		{
 			System.out.print("Stripe " + (char)(65 + i) + ": " + stripeSpeeds[i] + " Mbps, ");
 		}
 		
-		System.out.println();
+		boolean dataAvailable = true;
+		for (int i = 0; i < downHosts.length; i++)
+		{
+			if (stripeSpeeds[i] == 0)
+			{
+				dataAvailable = false;
+				break;
+			}
+		}
+		
+		System.out.println("\nIs all data available? " + dataAvailable);
+	}
+	
+	public static boolean allDataAvailable(Server[][] servers, int utcTime) //prints single hour
+	{
+		boolean[][] downHosts = new boolean[servers.length][servers[0].length];
+		int[] stripeSpeeds = new int[servers.length];
+		
+		for (int i = 0; i < downHosts.length; i++)
+		{
+			for (int g = 0; g < downHosts[i].length; g++)
+			{
+				downHosts[i][g] = !servers[i][g].uptimeScheme.isOnline(utcTime);
+			}
+		}
+		
+		for (int i = 0; i < downHosts.length; i++)
+		{
+			for (int g = 0; g < downHosts[i].length; g++)
+			{
+				//String res;
+				if (!downHosts[i][g])
+				{
+					//res = "";
+					stripeSpeeds[i] += 1;
+				}
+				//System.out.print(res);
+			}
+		}
+		
+		
+		boolean dataAvailable = true;
+		for (int i = 0; i < downHosts.length; i++)
+		{
+			if (stripeSpeeds[i] == 0)
+			{
+				dataAvailable = false;
+				break;
+			}
+		}
+		
+		return dataAvailable;
+		//System.out.println("\nIs all data available? " + dataAvailable);
 	}
 	
 	public static void testA() //all hosts will be somewhere in U.S.A. (-5:00 to -8:00 UTC)
 	{
-		int[] hostTimezoneArray = new int[256]; //24 hosts, not hours
-		Server[][] servers = new Server[4][6]; //4 stripes, 6 mirrors
+		int[] hostTimezoneArray = new int[120]; //24 hosts, not hours
+		Server[][] servers = new Server[4][30]; //4 stripes, 30 mirrors
 		double uptimeVariance = 0.3;
 		
 		for (int i = 0; i < hostTimezoneArray.length; i++)
@@ -102,5 +156,94 @@ public class ServerNetwork
 	}
 
 	
+	public static void testB() //all hosts will be somewhere in North America or Europe (-8:00 to +2:00 UTC)
+	{
+		int[] hostTimezoneArray = new int[120]; //24 hosts, not hours
+		Server[][] servers = new Server[4][30]; //4 stripes, 30 mirrors
+		double uptimeVariance = 0.3;
+		
+		for (int i = 0; i < hostTimezoneArray.length; i++)
+		{
+			int randomTime = -8; //PST
+			randomTime += (int)(Math.random() * 10); //values: -3, -2, -1, and 0
+			hostTimezoneArray[i] = randomTime;
+		}
+		
+		int sID = 0;
+		
+		for (int s = 0; s < servers.length; s++)
+		{
+			for (int m = 0; m < servers[s].length; m++, sID++)
+			{
+				UptimeScheme currUS = new UptimeScheme(hostTimezoneArray[sID], uptimeVariance);
+				int currPort = 4000 + (sID * 10);
+				
+				Server currS = new Server(currPort, currPort + 1, currPort + 9, (char)(65 + s), m, currUS);
+				new Thread(currS).start();
+				servers[s][m] = currS;
+			}
+		}
+		
+		Logs.info("ServerNetwork.testA(): All 24 servers started up.");
+		Logs.info("ServerNetwork.testA(): To test some uptimes, enter a range of numbers. Example: \'10 16\' test from 10:00 AM to 4:00 PM.");
+		Logs.info("ServerNetwork.testA(): To test a specific hour, enter a single number.");
+		
+		Scanner kbd = new Scanner(System.in);
+		while (true)
+		{
+			String currL = kbd.nextLine();
+			int num = Integer.parseInt(currL);
+			printUptimes(servers, num);
+			System.out.println("\n");
+		}
+	}
 	
+	public static void testB_100k() //all hosts will be somewhere in North America or Europe (-8:00 to +2:00 UTC)
+	{
+		int[] hostTimezoneArray = new int[120]; //24 hosts, not hours
+		Server[][] servers = new Server[4][30]; //4 stripes, 30 mirrors
+		double uptimeVariance = 0.3;
+		
+		for (int i = 0; i < hostTimezoneArray.length; i++)
+		{
+			int randomTime = -8; //PST
+			randomTime += (int)(Math.random() * 10); //values: -3, -2, -1, and 0
+			hostTimezoneArray[i] = randomTime;
+		}
+		
+		int sID = 0;
+		
+		for (int s = 0; s < servers.length; s++)
+		{
+			for (int m = 0; m < servers[s].length; m++, sID++)
+			{
+				UptimeScheme currUS = new UptimeScheme(hostTimezoneArray[sID], uptimeVariance);
+				int currPort = 4000 + (sID * 10);
+				
+				Server currS = new Server(currPort, currPort + 1, currPort + 9, (char)(65 + s), m, currUS);
+				new Thread(currS).start();
+				servers[s][m] = currS;
+			}
+		}
+		
+		Logs.info("ServerNetwork.testA(): All 24 servers started up.");
+		Logs.info("ServerNetwork.testA(): To test some uptimes, enter a range of numbers. Example: \'10 16\' test from 10:00 AM to 4:00 PM.");
+		Logs.info("ServerNetwork.testA(): To test a specific hour, enter a single number.");
+		
+		Scanner kbd = new Scanner(System.in);
+
+		String currL = kbd.nextLine();
+		int num = Integer.parseInt(currL);
+		//printUptimes(servers, num);
+		System.out.println("\n");
+		
+		int unavailTimes = 0;
+		for (int i = 0; i < 100000; i++)
+		{
+			if (!allDataAvailable(servers, num))
+				unavailTimes++;
+		}
+		
+		System.out.println("100000 iterations, tiems unavailable: " + unavailTimes);
+	}
 }
